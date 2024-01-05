de1dc7ba0fca9a252b9c92eb1f5c632a3af7a11c
ethanjoby <63834860+ethanjoby@users.noreply.github.com>
2024-01-05 00:05:44 -0800

    Add files via upload

--- replace_audio.py
+++ replace_audio.py
@@ -10,6 +10,30 @@ def display_help():
     print("  -h  Show this help message and exit.")
 
 
+
+def calculate_speed_factor():
+    video_file = "video.mp4"
+    audio_file = "video1.mp3"
+
+    if not (os.path.exists(video_file) and os.path.exists(audio_file)):
+        print(f'Video file {video_file} or audio file {audio_file} not found.')
+        return
+
+    video = VideoFileClip(video_file)
+    audio = AudioFileClip(audio_file)
+    return audio.duration/video.duration
+        
+import subprocess
+input_file = "video1.mp3"
+output_file = "video.mp3"
+speed_factor = calculate_speed_factor()
+
+ffmpeg_command = [
+    "ffmpeg",
+    "-i", input_file,
+    "-filter:a", f"atempo={speed_factor}",
+    output_file
+]
 def replace_audio():
     audio_extensions = ['.mp3', '.wav']
     video_extensions = ['.mp4']
@@ -35,7 +59,7 @@ def replace_audio():
 
             video = VideoFileClip(video_file)
             audio = AudioFileClip(audio_file)
-
+            
             video_with_new_audio = video.set_audio(audio)
             video_with_new_audio.write_videofile(os.path.join(current_folder, f'{basename}_ad.mp4'))
 
@@ -53,6 +77,8 @@ if __name__ == '__main__':
     if '-h' in sys.argv:
         display_help()
         sys.exit(0)
-
+        
+    print('Speed Factor:' + str(calculate_speed_factor()))
+    subprocess.run(ffmpeg_command)
     replace_audio()
 
