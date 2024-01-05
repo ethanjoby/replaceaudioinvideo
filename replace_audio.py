cd51d6da880a8e450516592acf01c94a2f7a8fc7
ethanjoby <63834860+ethanjoby@users.noreply.github.com>
2024-01-05 00:26:10 -0800

    Add files via upload

--- replace_audio.py
+++ replace_audio.py
@@ -35,37 +35,18 @@ ffmpeg_command = [
     output_file
 ]
 def replace_audio():
-    audio_extensions = ['.mp3', '.wav']
-    video_extensions = ['.mp4']
-    processed_audio_files = set()
-
     current_folder = os.getcwd()
+    video_file = "video.mp4"
+    audio_file = "video.mp3"
 
-    for file in os.listdir(current_folder):
-        if any(file.endswith(ext) for ext in video_extensions):
-            video_file = os.path.join(current_folder, file)
-            basename = os.path.splitext(file)[0]
+    if not (os.path.exists(video_file) and os.path.exists(audio_file)):
+        print(f'Video file {video_file} or audio file {audio_file} not found.')
+        return
 
-            audio_file = None
-            for ext in audio_extensions:
-                if os.path.exists(os.path.join(current_folder, f'{basename}{ext}')):
-                    audio_file = os.path.join(current_folder, f'{basename}{ext}')
-                    processed_audio_files.add(audio_file)
-                    break
-
-            if audio_file is None:
-                print(f'No matching audio file found for {file}')
-                continue
-
-            video = VideoFileClip(video_file)
-            audio = AudioFileClip(audio_file)
-            
-            video_with_new_audio = video.set_audio(audio)
-            video_with_new_audio.write_videofile(os.path.join(current_folder, f'{basename}_ad.mp4'))
-
-    for file in os.listdir(current_folder):
-        if any(file.endswith(ext) for ext in audio_extensions) and os.path.join(current_folder, file) not in processed_audio_files:
-            print(f'No matching video file found for {file}')
+    video = VideoFileClip(video_file)
+    audio = AudioFileClip(audio_file)
+    video_with_new_audio = video.set_audio(audio)
+    video_with_new_audio.write_videofile(os.path.join(current_folder, f'video_ad.mp4'))
 
 
 if __name__ == '__main__':
