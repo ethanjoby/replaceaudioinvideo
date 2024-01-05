870d89e7a26c370ee1c36c95db0e8b3c1d670b2e
ethanjoby <63834860+ethanjoby@users.noreply.github.com>
2024-01-05 00:31:18 -0800

    Add files via upload

--- replace_audio.py
+++ replace_audio.py
@@ -3,11 +3,6 @@ import os
 from moviepy.editor import *
 
 
-def display_help():
-    print("Usage: python replace_audio.py [-h]")
-    print("\nReplace the audio in a video file (mp4) with a provided audio file (wav or mp3).")
-    print("Arguments:")
-    print("  -h  Show this help message and exit.")
 
 
 
@@ -49,16 +44,7 @@ def replace_audio():
     video_with_new_audio.write_videofile(os.path.join(current_folder, f'video_ad.mp4'))
 
 
-if __name__ == '__main__':
-    if len(sys.argv) != 1:
-        print("Error: This script does not take any command-line arguments.")
-        display_help()
-        sys.exit(1)
-
-    if '-h' in sys.argv:
-        display_help()
-        sys.exit(0)
-        
+if __name__ == '__main__':      
     print('Speed Factor:' + str(calculate_speed_factor()))
     subprocess.run(ffmpeg_command)
     replace_audio()
